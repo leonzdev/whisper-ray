91719804356e647b072e550819fcda62bf9da3fc
leonzdev <leonzdev@leonz.dev>
2024-07-11 15:31:55 -0400

    Move things around

--- main.py
+++
@@ -1,6 +0,0 @@
-import os
-# Deploy the API
-from deployment.api import APIIngress
-from deployment.transcribe import TranscribeService
-
-whisper_ray = APIIngress.bind(TranscribeService.bind(os.getenv("MODEL_NAME"), os.getenv("MODEL_DEVICE")))
