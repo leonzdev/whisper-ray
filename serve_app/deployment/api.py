010443c1c821028b2bff5dff99243512ee28ba0c
leonzdev <leonzdev@leonz.dev>
2024-07-14 22:16:20 -0400

    Refactoring and add support for word level timestamp

--- serve_app/deployment/api.py
+++ serve_app/deployment/api.py
@@ -1,26 +1,48 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from typing import List
+from fastapi import FastAPI, UploadFile, File, Form, Request
+from fastapi.datastructures import FormData
 from ray import serve
 from ray.serve.handle import DeploymentHandle
-from .transcribe import TranscribeService, TranscribeInput
+from ..common.constants import FORMAT_JSON, SEGMENT
+from ..common.types import TranscribeInput
+
 app = FastAPI()
 
 @serve.deployment
 @serve.ingress(app)
 class APIIngress:
-    def __init__(self, transcribe_service: DeploymentHandle) -> None:
-        self.transcribe_service = transcribe_service
+    def __init__(self, whisper_model_service: DeploymentHandle) -> None:
+        self.transcribe_service = whisper_model_service
 
-    @app.post("/transcribe")
-    async def transcribe(self, file: UploadFile = File(...), model: str = Form(...), prompt: str = Form(None), temperature: float = Form(0.0), language: str = Form(None), format: str = Form("json")):
-        # Read the file content
+    @staticmethod
+    def parse_timestamp_granularities(form_data: FormData) -> List[str]:
+        timestamp_granularities: List[str] = form_data.getlist("timestamp_granularities[]")
+        if not timestamp_granularities:
+            return [SEGMENT]
+        return timestamp_granularities
+
+    @app.post("/v1/audio/transcriptions")
+    async def transcribe(self, 
+        request: Request,
+        file: UploadFile = File(...), 
+        model: str = Form(...), 
+        language: str = Form(None), 
+        prompt: str = Form(None), 
+        response_format: str = Form(FORMAT_JSON),
+        temperature: float = Form(0.0),
+        # timestamp_granularities: This parameter needs to be parsed from request directly
+    ):
+        form_data = await request.form()
         file_content = await file.read()
+        timestamp_granularities: List[str] = self.parse_timestamp_granularities(form_data)
         input_data = TranscribeInput(
             file=file_content,
             model=model,
             prompt=prompt,
             temperature=temperature,
             language=language,
-            format=format
+            response_format=response_format,
+            timestamp_granularities=timestamp_granularities
         )
         # Call the transcription service
         result = await self.transcribe_service.transcribe.remote(input_data)
